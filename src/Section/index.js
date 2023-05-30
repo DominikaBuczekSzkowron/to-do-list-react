4e75fa4a7b7457194531895b43ff56a65f7b9870
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-05-30 11:36:20 +0100

    add new line

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
 
-        <div className="section__body section__alignHederAndButtons">
+        <div className="section__body section__alignHeaderAndButtons">
             <h2 className="section__header">{title}</h2>
             {extraHeaderContent}
 
