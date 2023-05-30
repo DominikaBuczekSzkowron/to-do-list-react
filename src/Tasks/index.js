936e9ba856e40fece5976647feb1183cc5f86a84
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-05-30 13:06:53 +0100

    toggle task dane interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,29 +1,29 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="section__list">
         {tasks.map(task => (
             <li
                 key={task.id}
-                className={`section__list--item ${
-                    task.done && hideDone
+                className={`section__list--item ${task.done && hideDone
                         ? "section__list--hideItem"
                         : ""} `
                 }
             >
-
                 <button
-                    className="section__form--buttonDone">{
+                    className="section__form--buttonDone"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
+                    {
                         task.done
                             ? "✔"
                             : ""}
                 </button>
 
-                <span 
-                className={`section__list--text ${
-                    task.done
-                    ? " section__list--doneItem"
-                    : ""}`}>{task.content}
+                <span
+                    className={`section__list--text ${task.done
+                            ? " section__list--doneItem"
+                            : ""}`}>{task.content}
                 </span>
 
                 <button
