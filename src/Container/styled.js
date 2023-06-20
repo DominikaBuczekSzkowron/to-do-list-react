1fc8ee583bccb427137eacfe960d7601da36ae4b
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-06-20 14:07:29 +0100

    styled component Container

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,6 +1,8 @@
-.container {
+import styled, { css } from "styled-components";
+
+export const Main = styled.main`
   font-family: "Montserrat", sans-serif;
   margin: 0 auto;
   max-width: 1000px;
   padding: 20px;
-}
+`;
