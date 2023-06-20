1f4095d7b4d2d93bb80db867163ad1c1a530174c
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-06-20 14:06:41 +0100

    code format

--- src/App.js
+++ src/App.js
@@ -15,13 +15,8 @@ function App() {
     setHideDone((hideDone) => !hideDone);
   };
 
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
+  const { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask } =
+    useTasks();
 
   return (
     <Container className="container">
