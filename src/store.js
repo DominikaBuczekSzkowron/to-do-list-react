bc7f33d10736d79c363516cd8c52281d251f21e6
Dominika Buczek-Szkowron <buczekszkowron@gmail.com>
2023-07-06 10:40:40 +0100

    create rootSaga

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import taskReducer from "./features/tasks/taskSlice";
-import { watchFetchExampleTasks } from "./features/tasks/taskSaga";
+import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -12,5 +12,5 @@ const store = configureStore({
   middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(watchFetchExampleTasks);
+sagaMiddleware.run(rootSaga);
 export default store;
